0945a57d9b84e5c76efc89e431a6bf3d0d61bf4f
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-27 21:58:19 +0530

    First Commit

--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -18,13 +18,17 @@ namespace LineComp
             Console.WriteLine("Line 2 coordinates point 2  (x,y)");
             int x4 = Convert.ToInt32(Console.ReadLine());
             int y4 = Convert.ToInt32(Console.ReadLine());
-            if (Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2).Equals(Math.Sqrt((x3 - x4) ^ 2 + (y3 - y4) ^ 2)))
+            if (Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2).CompareTo(Math.Sqrt((x3 - x4)^ 2 + (y3 - y4)^ 2))==0)
             {
                 Console.WriteLine("The lengths of two lines are equal");
             }
+            else if(Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2).CompareTo(Math.Sqrt((x3 - x4) ^ 2 + (y3 - y4) ^ 2))< 0)
+            {
+                Console.WriteLine("Line 1 is greater");
+            }
             else
             {
-                Console.WriteLine("The lengths of two lines are not equal");
+                Console.WriteLine("Line 2 is greater");
             }
 
         }
