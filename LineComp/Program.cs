492c15e75eb87990621d0cafd99574bd9a493c37
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-27 21:14:49 +0530

    First Commit

---
+++ LineComp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LineComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter coordinates point 1 (x,y)");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter coordinates point 2  (x,y)");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2));
+        }
+    }
+}
