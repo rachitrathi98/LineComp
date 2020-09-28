f0d290d7df8e9e01abc8531238d5f03b6a239783
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-28 08:36:45 +0530

    First Commit

--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -6,23 +6,26 @@ namespace LineComp
     {
         static void Main(string[] args)
         {
+            int x1, x2, y1, y2;
             Console.WriteLine("Line 1 coordinates point 1 (x,y)");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Line 1 coordinates point 2  (x,y)");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double d1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
             Console.WriteLine("Line 2 coordinates point 1 (x,y)");
-            int x3 = Convert.ToInt32(Console.ReadLine());
-            int y3 = Convert.ToInt32(Console.ReadLine());
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Line 2 coordinates point 2  (x,y)");
-            int x4 = Convert.ToInt32(Console.ReadLine());
-            int y4 = Convert.ToInt32(Console.ReadLine());
-            if (Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2).CompareTo(Math.Sqrt((x3 - x4)^ 2 + (y3 - y4)^ 2))==0)
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double d2 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            if (d1==d2)
             {
                 Console.WriteLine("The lengths of two lines are equal");
             }
-            else if(Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2).CompareTo(Math.Sqrt((x3 - x4) ^ 2 + (y3 - y4) ^ 2))< 0)
+            else if(d1>d2)
             {
                 Console.WriteLine("Line 1 is greater");
             }
